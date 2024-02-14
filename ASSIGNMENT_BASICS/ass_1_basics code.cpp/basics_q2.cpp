2d33c5a016bbf003a97b7b9ae1deea61d5b42cf7
SATYAM96619 <159725489+SATYAM96619@users.noreply.github.com>
2024-02-14 12:36:30 +0530

    Add files via upload

---
+++ ASSIGNMENT_BASICS/ass_1_basics code.cpp/basics_q2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the two numbers";
+    float a,b;
+    cin>>a>>b;
+    if(a<6 && b<6){
+        cout<<a+b;
+    }
+    return 0;
+}
